c949eb5c178397a03a56d343e6a8d061c010d5a1
maxklenk <maxklenk@users.noreply.github.com>
2020-03-30 11:51:31 +0000

    Deploying to gh-pages from  - bc3b071390e2f686bb0df2584cdd41cf18425d23 🚀

---
+++ precache-manifest.1d6968942eb62136e1054af0edc65b24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ee1386f572676f19bf56f53ab506ca",
+    "url": "/clockify-switch/index.html"
+  },
+  {
+    "revision": "1ef3fc136efc4452a191",
+    "url": "/clockify-switch/static/css/main.5e9a582a.chunk.css"
+  },
+  {
+    "revision": "de707f9759d9d4ab19fa",
+    "url": "/clockify-switch/static/js/2.b82991f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clockify-switch/static/js/2.b82991f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef3fc136efc4452a191",
+    "url": "/clockify-switch/static/js/main.6c6c88f4.chunk.js"
+  },
+  {
+    "revision": "fc2cff083e65f801c5bf",
+    "url": "/clockify-switch/static/js/runtime-main.866f976f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab5e3029547989adff78ffe8602434b1.js"
+  "/clockify-switch/precache-manifest.1d6968942eb62136e1054af0edc65b24.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clockify-switch/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
